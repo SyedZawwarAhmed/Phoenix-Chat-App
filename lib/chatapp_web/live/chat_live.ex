3f87b1c26b334f0e858ab6987b840989c19195a8
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2025-04-30 00:36:41 +0500

    feat: add user in the list when a user joins

--- lib/chatapp_web/live/chat_live.ex
+++ lib/chatapp_web/live/chat_live.ex
@@ -1,7 +1,13 @@
 defmodule ChatappWeb.ChatLive do
   use ChatappWeb, :live_view
+  require Logger
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      random_user_id = Enum.random(1..100)
+      Chatapp.UserStore.add_user(random_user_id)
+      Logger.info("Adding user #{random_user_id} on WebSocket connection")
+    end
     users = Chatapp.UserStore.get_users()
     {:ok, assign(socket, users: users)}
   end
